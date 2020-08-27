30063c8a60f7f13029a64d7d82b5136de95cc6c6
Saahil <saahil@Saahils-MacBook-Air.local>
2020-08-27 18:43:44 +0100

    i still dont like u

--- Landing/landing.php
+++ Landing/landing.php
@@ -1,7 +1,3 @@
 <html>
-<p> jam is poo !! </p>
 
-<h>
-hi poo poo poo poo poo poo poo poo poo po oopo opoopopo pooopooopooopooopopoppoopoopooopooopooo poopo pop opoopo poppopopoopo po popopopopopopopoopopoo
-</h>
 </html>
